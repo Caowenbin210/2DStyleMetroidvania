21dce54b3ee7e96442d0b394cb79b416e0f61513
CaoXuyang <210675154@qq.com>
2026-03-05 18:52:38 +0800

    25. 无尽的背景（原理与实现）

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -6,23 +6,38 @@ public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
-         
+    private float cameraHalfWidth;
+
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect; // 摄像机一半的长度等于正交尺寸乘宽高比例
+        CalculateImageLength();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         float currentCameraPositionX = mainCamera.transform.position.x;
         float distanceToMove = currentCameraPositionX - lastCameraPositionX;
         lastCameraPositionX = currentCameraPositionX;
 
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
         foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void CalculateImageLength()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }
