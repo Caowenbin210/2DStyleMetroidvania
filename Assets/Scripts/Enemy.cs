b328f81c9c9b4d1f6c9e4711fa76c7aba812040a
CaoXuyang <210675154@qq.com>
2026-04-10 00:17:00 +0800

    30. 给骷髅敌人增加待机和移动状态

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-    
+    public Enemy_Idle idleState;
+    public Enemy_MoveState moveState;
+
+    [Header("ÒÆ¶¯Ï¸½Ú")]
+    public float idleTime = 2;
+    public float moveSpeed = 1.4f;
+    [Range(0f, 2f)]
+    public float moveAnimSpeedMultiplier = 1;
 }
--- Assets/Scripts/Enemy_Skeleton.cs
+++ Assets/Scripts/Enemy_Skeleton.cs
@@ -2,7 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Skeleton : Entity
+public class Enemy_Skeleton : Enemy
 {
-    
+    protected override void Awake()
+    {
+        base.Awake();
+
+        idleState = new Enemy_Idle(this, stateMachine, "idle");
+        moveState = new Enemy_MoveState(this, stateMachine, "move");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        stateMachine.Initialize(idleState);
+    }
 }
